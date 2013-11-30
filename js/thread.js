fc3e7f7aac2ba8b454874b2f4827b27730fda604
Dan Klco <dan.klco@sixdimensions.com>
2013-11-29 21:04:07 -0500

    Separating out the thread JS into a separate file

---
+++ js/thread.js
@@ -0,0 +1,42 @@
+---
+layout: none
+---
+
+var qs = (function(a) {
+    if (a == "") return {};
+    var b = {};
+    for (var i = 0; i < a.length; ++i)
+    {
+        var p=a[i].split('=');
+        if (p.length != 2) continue;
+        b[p[0]] = decodeURIComponent(p[1].replace(/\+/g, " "));
+    }
+    return b;
+})(window.location.search.substr(1).split('&'));
+var categoryId = qs['category'];
+var threadName = qs['thread']
+
+// Set the Disqus Variables
+var disqus_shortname = '{{ site.disqus_shortname }}'; 
+var disqus_identifier = threadName;
+var disqus_title = threadName;
+var disqus_category_id = categoryId;
+var disqus_url = '{{ site.url }}/thread.html?thread=' + encodeURIComponent(qs['thread']) + '&category=' + categoryId;
+
+// Update the page based on the thread
+document.title = threadName + ' | ' + document.title;
+$('.thread-title').html(threadName);
+
+// Get the category data
+$.getJSON('{{ site.baseurl }}', function(categories){
+	var category = categories[categoryId];
+	$('.category-link').html(category.title);
+	$('.category-link').attr('href', category.url);
+});
+
+// Include the Disqus Component
+(function() {
+    var dsq = document.createElement('script'); dsq.type = 'text/javascript'; dsq.async = true;
+    dsq.src = '//' + disqus_shortname + '.disqus.com/embed.js';
+    (document.getElementsByTagName('head')[0] || document.getElementsByTagName('body')[0]).appendChild(dsq);
+})();
